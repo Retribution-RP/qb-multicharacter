a9ea3e30b809ebdb25efc77ce51f1e2ad2f96712
jj_case-gaming <jjcasegaming@yahoo.com>
2022-12-18 11:21:12 -0800

    changed to 3 characters

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates 
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
 Config.customNationality = false -- Defines if Nationality input is custom of blocked to the list of Countries
 
-Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
+Config.DefaultNumberOfCharacters = 3 -- Define maximum amount of default characters (maximum 5 characters defined by default)
 Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
     { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
 }
